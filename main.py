ac4bee755d4739d40b8e1e2210f553a9e29138f7
vicebaraban <varl.avgust@yandex.ru>
2022-07-02 19:48:16 +0300

    added player lose animation, lose menu

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ _main_menu_sprites = pygame.sprite.Group()
 _pause_menu_sprites = pygame.sprite.Group()
 _player_sprites = pygame.sprite.Group()
 _playing_sprites = pygame.sprite.Group()
+_lose_menu_sprites = pygame.sprite.Group()
 
 
 FPS = 60
 
+kostil = True
 count_items = 0
 lose = False
 health = 0
@@ -23,7 +25,15 @@ gcd = 0
 images = {'player': pygame.transform.scale(pygame.image.load('images/player2.png'), (168, 272)),
           'player2': pygame.transform.scale(pygame.image.load('images/player3.png'), (168, 272)),
           'player3': pygame.transform.scale(pygame.image.load('images/player4.png'), (168, 272)),
+          'vano1': pygame.transform.scale(pygame.image.load('images/vano1.png'), (168, 272)),
+          'vano2': pygame.transform.scale(pygame.image.load('images/vano2.png'), (168, 272)),
+          'vano3': pygame.transform.scale(pygame.image.load('images/vano3.png'), (168, 272)),
+          'vano4': pygame.transform.scale(pygame.image.load('images/vano4.png'), (168, 272)),
+          'vano5': pygame.transform.scale(pygame.image.load('images/vano5.png'), (168, 272)),
+          'vano6': pygame.transform.scale(pygame.image.load('images/vano6.png'), (168, 272)),
           'background_main_menu': pygame.transform.scale(pygame.image.load('images/background_main_menu.jpg'),
+                                                         (800, 600)),
+          'background_lose_menu': pygame.transform.scale(pygame.image.load('images/background_lose_menu.jpeg'),
                                                          (800, 600)),
           'background_wings': pygame.transform.scale(pygame.image.load('images/background_playing_wings.png'),
                                                      (800, 600)),
@@ -58,6 +68,7 @@ class GameState(Enum):
     MAIN_MENU = auto()
     PLAYING = auto()
     PAUSE = auto()
+    LOSE = auto()
 
 
 class Game:
@@ -70,6 +81,7 @@ class Game:
         pygame.mouse.set_visible(False)
         self._init_main_menu()
         self.playing_mode = 0
+        self._animator = Animator()
 
     def run(self):
         self.running = True
@@ -96,6 +108,8 @@ class Game:
             self._playing_process_events(self._events)
         elif self.game_state == GameState.PAUSE:
             self._pause_menu_process_events(self._events)
+        elif self.game_state == GameState.LOSE:
+            self._lose_menu_process_events(self._events)
 
     def _main_menu_process_events(self, events):
         for event in events:
@@ -129,6 +143,15 @@ class Game:
                     self.game_state = GameState.MAIN_MENU
                     self._init_main_menu()
 
+    def _lose_menu_process_events(self, events):
+        for event in events:
+            if event.type == pygame.QUIT:
+                self._terminate()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self._button_menu.in_mouse():
+                    self.game_state = GameState.MAIN_MENU
+                    self._init_main_menu()
+
     def _playing_process_events(self, events):
         for event in events:
             if event.type == pygame.QUIT:
@@ -145,6 +168,13 @@ class Game:
     def _init_pause_menu(self):
         self._button_play = Button(images['button_play'], images['button_play_on'], (320, 300), _pause_menu_sprites)
         self._button_menu = Button(images['button_exit'], images['button_exit_on'], (400, 300), _pause_menu_sprites)
+
+    def _init_lose_menu(self):
+        self._init_lose_animation()
+        self._button_menu = Button(images['button_exit'], images['button_exit_on'], (370, 400), _lose_menu_sprites)
+
+    def _init_lose_animation(self):
+        self._animator.add_animation(Animation(self._player, 30, 3, [images[f'vano{i}'] for i in range(1, 7)]))
 
     def _init_main_menu(self):
         _all_sprites.update(kill=True)
@@ -166,8 +196,13 @@ class Game:
         self._button_clear = Button(images['button_clear'], images['button_clear'], (700, 40), _playing_sprites)
 
     def _render_playing(self):
-        global cd, gcd
+        global cd, gcd, health, kostil
         _playing_sprites.update()
+        if health == 3 and kostil:
+            kostil = False
+            # health = 0
+            # self.game_state = GameState.LOSE
+            self._init_lose_menu()
         if cd == 0:
             gcd = random.randint(60, 140)
             cd = 1
@@ -187,13 +222,23 @@ class Game:
 
     def _render_screen(self):
         # self._screen.blit()
+        self._animator.update()
         if self.game_state == GameState.MAIN_MENU:
             self._render_main_menu()
-        if self.game_state == GameState.PLAYING:
+        elif self.game_state == GameState.PLAYING:
             self._render_playing()
-        if self.game_state == GameState.PAUSE:
+        elif self.game_state == GameState.PAUSE:
             self._render_pause_menu()
+        elif self.game_state == GameState.LOSE:
+            self._render_lose_menu()
         pygame.display.flip()
+
+    def _render_lose_menu(self):
+        _lose_menu_sprites.update()
+        self._screen.blit(images['background_lose_menu'], (0, 0))
+        _lose_menu_sprites.draw(self._screen)
+        if pygame.mouse.get_focused():
+            self._screen.blit(images['cursor'], pygame.mouse.get_pos())
 
     def _render_main_menu(self):
         _main_menu_sprites.update()
@@ -211,6 +256,41 @@ class Game:
             self._screen.blit(images['cursor'], pygame.mouse.get_pos())
 
 
+class Animator:
+    def __init__(self):
+        self.storage_animations = []
+        self._urn = []
+
+    def update(self):
+        for animation in self.storage_animations:
+            if animation.update() == 0:
+                self._urn.append(animation)
+        if self.storage_animations and self._urn:
+            for animation in self._urn:
+                self.storage_animations.remove(animation)
+
+    def add_animation(self, animation):
+        self.storage_animations.append(animation)
+
+
+class Animation:
+    def __init__(self, obj, speed, time, anim_images):
+        self.images = anim_images
+        self.obj = obj
+        self.speed = speed
+        self.time = time
+        self.counter = 0
+        print(1)
+
+    def update(self):
+        if self.counter % self.speed == 0:
+            self.obj.change_image(self.images[self.counter // self.speed])
+        self.counter += 1
+        if self.counter == FPS * self.time - 1:
+            return 0
+        return 1
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, sprite_img, sprite_img2, sprite_img3, *groups):
         super().__init__(_all_sprites, *groups)
@@ -223,13 +303,16 @@ class Player(pygame.sprite.Sprite):
         self.image = sprite_img
         self.rect = self.image.get_rect().move(self.x, self.y)
 
+    def change_image(self, img):
+        self.image = img
+
     def update(self, kill=False):
         global health
         if kill:
             self.kill()
-        if health == 3:
+        if health == 1:
             self.image = self.sprite_img2
-        elif health == 6:
+        elif health == 2:
             self.image = self.sprite_img3
         self.y = pygame.mouse.get_pos()[1]
         self.rect = self.image.get_rect().move(self.x, self.y)
@@ -260,9 +343,7 @@ class Item(pygame.sprite.Sprite):
             if self.y in range(player_y - 100, player_y - 20) and not self.out:
                 self.kill()
                 count_items += 1
-                print(count_items)
             else:
-                print('lose')
                 health += 1
                 self.out = True
 
